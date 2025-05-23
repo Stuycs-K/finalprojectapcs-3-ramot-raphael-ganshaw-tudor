4c8f86a17b35f7f8af74e4d507a5177226d55e3e
Raphael Ramot <raphaelr20@nycstudents.net>
2025-05-23 09:45:32 -0400

    basic pacman class

--- Pacman.java
+++ Pacman.java
@@ -1,3 +1,32 @@
 public class Pacman {
-    
+    private int[] location;
+    private int direction;
+    private boolean poweredUp;
+
+    public Pacman(int[] loc, int dir) {
+        location = loc;
+        direction = dir;
+        poweredUp = false;
+    }
+
+    public void changeLocation(int[] loc) {
+        location = loc;
+    }
+    public int[] getLocation() {
+        return location;
+    }
+
+    public void changeDirection(int dir) {
+        direction = dir;
+    }
+    public int getDirection() {
+        return direction;
+    }
+
+    public void powerUp(boolean pow) {
+        poweredUp = pow;
+    }
+    public boolean isPoweredUp() {
+        return poweredUp;
+    }
 }
